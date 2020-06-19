8ff695b77feb44fae83c446d4571ce927efe3a5d
AlexandreAdabo <alexandreadabo_@hotmail.com>
2020-06-19 20:12:39 -0300

    Atividade 2 feita

--- atividade_2/index.js
+++ atividade_2/index.js
@@ -1,5 +1,12 @@
 // ## Importe a biblioteca ##
+let rs = require('readline-sync')
+let mascara = rs.question ("Você está usando máscara ?")
+let lavarasmaos = rs.question ("Você está lavando as mãos frequentemente ?")
 
-
-
+if (mascara && lavarasmaos == "Sim") {
+    console.log ("Parabêns você está seguindo as orientações corretamente")
+} 
+else {
+    console.log ("Você não está agindo corretamente, use mascara e lave sempre as mãos")
+}
 // ## Faça o código ##
