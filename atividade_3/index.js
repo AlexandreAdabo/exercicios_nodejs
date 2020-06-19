416e182dc8a2323f4c8fd42bad2ff71bd979fc1e
AlexandreAdabo <alexandreadabo_@hotmail.com>
2020-06-19 20:22:28 -0300

    Atividade 3 feita

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,9 @@
 // ## Importe a biblioteca ##
+let rs = require('readline-sync')
 
+let ladoquadrado = rs.questionFloat("Qual o tamanho do lado do quadrado ?")
 
+let area = ladoquadrado *ladoquadrado
 
+console.log ("A área do quadrado é: "+ area)
 // ## Faça o código ##
